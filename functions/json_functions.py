97359ca1205076bddbe0580521c142997ba37804
Blender Defender <defenderblender@gmail.com>
2021-10-08 22:22:40 +0200

    Refactor: Add type annotations to json_functions.py

--- functions/json_functions.py
+++ functions/json_functions.py
@@ -23,13 +23,13 @@ import json
 from os import path as p
 
 
-def decode_json(path):
+def decode_json(path) -> dict:
     with open(path) as f:
         j = json.load(f)
     return j
 
 
-def encode_json(j, path):
+def encode_json(j, path) -> dict:
     with open(path, "w") as f:
         json.dump(j, f, indent=4)
     return j
