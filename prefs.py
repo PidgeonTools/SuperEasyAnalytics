df85d13aff11a7329023538285eb6897857fe926
Blender Defender <contact.blenderdefender@gmail.com>
2024-06-14 16:20:42 +0200

    style: Remove obsolete comments

--- prefs.py
+++ prefs.py
@@ -144,26 +144,12 @@ class SUPEREASYANALYTICS_APT_Preferences(AddonPreferences):
                     text="Enable Freelancer Statistics")
 
         if bpy.app.version < (4, 2):
-            # col = layout.column() # works best if a column, or even just self.layout
             mainrow = layout.row()
             col = mainrow.column()
 
-            # updater draw function
-            # could also pass in col as third arg
             addon_updater_ops.update_settings_ui(self, context)
 
-        # Alternate draw function, which is more condensed and can be
-        # placed within an existing draw function. Only contains:
-        #   1) check for update/update now buttons
-        #   2) toggle for auto-check (interval will be equal to what is set above)
-        # addon_updater_ops.update_settings_ui_condensed(self, context, col)
-
-        # Adding another column to help show the above condensed ui as one column
-        # col = mainrow.column()
-        # col.scale_y = 2
-        # col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
-
-        # Layout the discord URL.
+        # Layout the Discord URL.
         col = layout.column()
         op = col.operator("wm.url_open", text="Support", icon="URL")
         op.url = "https://bd-links.netlify.app/discord-sea"
